004c67cc76a944c865a86093a94dfc250fff4e8e
David Alimi <davealimi@gmail.com>
2024-05-13 13:47:40 +0300

    Add pre and post backup/restore scripts for PostgreSQL data

--- scripts/postRestore.sh
+++ scripts/postRestore.sh
@@ -1,2 +1,8 @@
 #Restart the stack after files are restored
 docker-compose up -d;
+sleep 5s;
+
+#restore from last dump
+gunzip < ./lastDump.sql.gz | docker-compose exec -T db psql -U postgres
+
+docker-compose exec -T db psql -U db -c "ALTER ROLE postgres WITH PASSWORD '$POSTGRES_PASSWORD';"
--- scripts/preBackup.sh
+++ scripts/preBackup.sh
@@ -1,1 +1,2 @@
-#nothing to do ...
+#save latest dump to disk
+docker-compose exec -T db pg_dumpall -c -U postgres | gzip > ./lastDump.sql.gz
